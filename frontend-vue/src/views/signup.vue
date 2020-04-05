54fa2020a76b770875e60cbf32b25ceec94991f1
peterta <peterta@ethz.ch>
2020-04-05 18:39:21 +0200

    added more stuff on the signup

--- frontend-vue/src/views/signup.vue
+++ frontend-vue/src/views/signup.vue
@@ -3,7 +3,7 @@
     <header>
       <img src="../assets/covid.jpg" id="covid">
       <h1>COVIDetector</h1>
-      <p class="lead">Help the world's scientific community!</p> 
+      <p class="lead">Help the world's scientific community!</p>
     </header>
     <article class="row justify-content-center">
       <div class="card col-10 col-md-8">
@@ -26,6 +26,26 @@
             </div>
           </form>
           <p>Already have a confirmation token? Click <router-link to="confirm">here</router-link></p>
+        </div>
+      </div>
+      <div class="card col-10 col-md-8">
+        <div class="card-body">
+          <h4>What does our service provide?</h4>
+          <br/>
+          Our aim is to provide the scientific community with a collection of <em>anonymized</em> voice data samples that will help further research efforts. In addition, this service aims to provide a mean to test users whether or not they will become sick in the upcoming days. This means, with COVIDetector, in the future, one can:
+          <br/>
+          <ul>
+            <li>Check the probability, using one of our machine learning models, the likelyhood they will become sick in the upcoming days</li>
+            <li>Provide a completely <em>anonymized</em> dataset that can be downloaded by anyone to further research studies</li>
+            <li>Open source all aspects of this project, including the website, backend, and the machine learning algorithms, so that the community can further improve on our work. <a href="https://github.com/andreatulimiero/COVIDetector">Link to the project on Github.</a></li>
+          </ul>
+          <p>However, to make this project come to light, we need voice samples collected from the public. We encourage you to sign up and upload a voice sample, and then once we have enough, we can properly train the model to start detection.</p>
+        </div>
+      </div>
+      <div class="card col-10 col-md-8">
+        <div class="card-body">
+          <h4>Medical Advice</h4>
+          <p>We hope that this service can be of the most use to the public. We envision that our product will work well and will correctly detect diseases early. However, this service should only serve to inform the user on whether or not they will be contagious in the upcoming days, and should not be used as a replacement for a doctor.</p>
         </div>
       </div>
     </article>
