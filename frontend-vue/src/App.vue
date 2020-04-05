5ec310f473d2d9e78279aaa720e0751a1bd0b312
peterta <peterta@ethz.ch>
2020-04-05 18:39:21 +0200

    added more stuff on the signup
    adding footer

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -4,19 +4,22 @@
     <main class="container">
       <router-view/>
     </main>
+    <myfooter></myfooter>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue'
+import MyFooter from '@/components/Footer.vue'
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    MyFooter
   },
   computed: {
-    showNavbar() { return this.$store.getters.isLoggedIn }
+    showNavbar() { return this.$store.getters.isLoggedIn },
   }
 }
 </script>
@@ -27,18 +30,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
