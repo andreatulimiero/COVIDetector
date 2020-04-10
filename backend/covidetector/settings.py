07c5385b74bfd28685ff6dbcc2e31e7bfdf90182
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2020-04-10 20:12:09 +0200

    Added OpenAPI endpoint

--- backend/covidetector/settings.py
+++ backend/covidetector/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_celery_results',
+    'rest_framework',
 
     'app',
     'corsheaders',
--- backend/covidetector/urls.py
+++ backend/covidetector/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 from rest_framework.routers import SimpleRouter
 
@@ -12,5 +13,10 @@ router.register(r'patients', views.PatientViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('openapi/', get_schema_view(
+        title="COVIDetector",
+        description="API for the COVIDetector project",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
